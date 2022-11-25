595cee51b1429f7e44afcd6d896336b54b884f29
Kevin <kevinmcmackin1@gmail.com>
2022-11-25 15:57:00 -0400

    first commit to programmer flash cards

---
+++ README.md
@@ -0,0 +1,26 @@
+# Programmer Interview Study
+#### Video Demo: https://www.youtube.com/watch?v=RB6xuGf8QeY
+#### Description:
+For my final project for CS50 I decided to create a site that users can use to help prepare them for their next coding-related interview. Users can press the "get question" button to get a question related to coding/programming. These questions are particular ones that are often asked in coding interviews. When the user wants to see the answer, they can press the "get answer" button for it to be revealed. This way, the site sort of acts in the same way that flashcards do.
+
+I decided to do this as my final project as it allowed me to practice and expand my knowledge of writing code using the languages JavaScript, HTML, and CSS.
+Before this project, I did not feel very comfortable with these three languages as we had only worked with them a bit at the end of CS50. In addition, most of the time that I was doing assignments with them, a lot of the hard work had already been done, so I didn't feel like I could stand on my own two feet with these languages at this point. When I was thinking of doing a final project, I thought a lot about making an app with swift, but instead opted to build on my knowledge of CSS, JS, and HTML, rather than learn something completely new. 
+
+When I started working on the project, I planned to make a more dynamic site, where there would be leaderboards and other cool features (it was going to be more of a game at this point rather than a study tool). But as I went down the rabbit hole of making a site like this, I realized that it was going to take a lot more time than anticipated if I was to do it like this. So I changed the plan to make it more of a study tool. This path that I started down was not a complete waste of time as I learned Git, GitHub hosting with GitHub Pages, and learned a little about hosting with Google Firebase before changing the direction of the project.
+
+Because I made the site without the use of databases, all the questions and answers used were instead stored in arrays in JavaScript. As I learn more about coding, more questions will be added to the study tool. By doing the project this way I was able to practice using these coding languages, I learned more about making a website and learned more coding concepts which I had to learn to make questions about.
+
+Overall, I am proud of what I was able to create, and I learned a lot from this project and the class as a whole.
+
+## Files
+### .html
+The .html file contains the code which displays the website. The site contains a title, some text, two buttons, and some text boxes for the questions and answers which are passed to it by the JS file.
+
+### .css
+This file contains the styling code for the site. Overall, doing the styling was my favourite part of the project as it was fun to see the site come alive.
+
+### .js
+The JavaScript file contains the brains of the site. This file includes funcitons which get the question and answer, hides elements, etc. It also contains the array of the questions and an array of the answers.
+
+The video can be found at:
+https://www.youtube.com/watch?v=RB6xuGf8QeY
---
+++ script.js
@@ -0,0 +1,106 @@
+function toggleText() {
+    var text = document.getElementById("demo");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function myToggle() {
+    var text = document.getElementById("demo");
+    if (text.style.display === "none") {
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+let used_questions = [];
+function randomQuestion() {
+  const len = questions.length;
+  const validQuesLength = valid_questions.length
+
+  /*check if all questions have been gone through*/
+  if (validQuesLength === used_questions.length) {
+    used_questions = [];
+  }
+
+  let rnd = Math.floor(Math.random() * len);
+
+  while (used_questions.indexOf(rnd) != -1 || valid_questions.indexOf(rnd) === -1) {
+    rnd = Math.floor(Math.random() * len);
+  }
+
+  document.getElementById('randomQuestion').value = questions[rnd].question;
+  document.getElementById('randomAnswer').value = questions[rnd].answer;
+  document.getElementById('myNum').value = len;
+
+  used_questions.push(rnd);
+
+  numer();
+}
+
+function numer() {
+  document.getElementById('numer').innerHTML = used_questions.length;
+}
+
+const questions = [
+  { question: "What is a data structure?", answer: "Data structures are storage formats that define the way that data is stored, organized, retrieved, processed manipulated, etc. Different types of data structures serve different purposes. For a deeper understanding, visit: https://discuss.boardinfinity.com/t/what-is-data-structure-and-it-types/12222/2" },
+  { question: "What is an array?", answer: "An array is a collection of similar-type data items stored at contiguous memory locations. It allows for data to be accessed only by its indexing number(s)." },
+  { question: "What is a linked list?", answer: "A linked list is a linear data structure in which the elements are NOT necessarily stored in a contiguous manner. The data elements are instead linked via 'pointers' which index the location of the following element in the sequence." },
+  { question: "What does LIFO stand for and what does it mean?", answer: "LIFO stands for last in, first out. It refers to a way in which data can be stored and retrieved. It means that data which was stored last will be extracted first." },
+  { question: "What does FIFO stand for and what does it mean?", answer: "FIFO stands for first in, first out. It refers to a way in which data can be stored and retrieved. It means that data which was stored first will be extracted first." },
+  { question: "What is a stack?", answer: "A stack is a data structure consisting of a set of homogeneous elements and is based on the principle of last in first out. It is a commonly used abstract data type with two major operations, push and pop which are carried out on the element at the top of the stack. The push operation adds an element to the top of the stack while the pop operation removes an element from the top position. An example of this in computer operations is web browsing. As you browse the web, the URL of the page that you are currently on is placed at the top of the stack, whereas the URL from where you started can be found at the base. When you hit the 'back' button, your browser moves through the stack starting from the top." },
+  { question: "What is a queue?", answer: "A queue is a linear data structure which stores elements in a first in, first out order. For more information on queues, as well as to learn more about different types of queues, visit: https://www.geeksforgeeks.org/queue-data-structure/ " },
+  { question: "What is a binary search tree?", answer: "A binary tree is an extension of the linked-list data structure where each 'node' has at most two 'children' and each of the 'children nodes' has two 'children nodes' and so forth. The tree begins at the top with the main node which then links to the left sub-tree (whose value is less than its parent node) and the right sub-tree (whose value is greater than its parent node). This data structure allows for speed and accuracy as the amount of key-value pair comparisons that must be made is decreased. For more information, including a detailed visual, visit: https://www.guru99.com/binary-search-tree-data-structure.html" },
+  { question: "What does a recursive function do?", answer: "A recursive function solves a particular problem by calling a copy of itself and solving smaller subproblems of the original problems, then combining them to get the desired solution as a whole. This is also referred to as the divide-and-conquer method." },
+  { question: "What are doubly linked lists?", answer: "A doubly linked list is a way of storing data in a non-contiguous manner. This is made possible by pointers that point to the location of the data that is next in the sequence. With doubly linked lists, navigation of the data is possible in both directions as each set of data has a pointer which contains the location of the succeeding data and a pointer which contains the location of the prior data." },
+  { question: "What is a graph?", answer: "A graph is a type of non-linear data structure that is used to store data in the form of nodes and edges/arcs. Fore more information, visit: https://afteracademy.com/blog/introduction-to-graph-in-programming#:~:text=A%20graph%20is%20a%20type,edges%20in%20the%20Graph%20G." },
+  { question: "What is a deque?", answer: "A deque is a double-ended queue in which data elements can be added or removed from either end (the front/head and the back/tail)." },
+  { question: "What's the difference between stacks and arrays?", answer: "A stack follows the last in, first out principle (LIFO) and so to access any of the data, you must start at the top of the stack. Arrays do not follow any specific order. Instead, data can be accessed by refering to the index of the element." },
+  
+  { question: "What are classes and objects? (OOP)", answer: "A class is a collection of objects which the user defines. Classes are types of data structures that make the data easier to sort, find, manipulate, etc. Objects are instances of a class created with specifically defined data." },
+  { question: "What are class constructors? (OOP)", answer: "Class constructors assign values to the data members of the class when an object of the class is created. For more information on OOP, as well as to find examples, visit: https://www.analyticsvidhya.com/blog/2020/09/object-oriented-programming/#:~:text=Now%2C%20there%20are%20four%20fundamental,in%20order%20to%20understand%20OOPs." },
+  { question: "What does OOP stand for and what is it?", answer: "OOP stands for object-oriented programming. This type of programming is centered around creating objects, or interrelated variables (property of the object) and functions (behavior of the object)." },
+  { question: "What is abstraction? (OOP)", answer: "Abstraction is a concept where only necessary attributes of a larger data pool are shown to the user. Unnecessary data is hidden from the users to reduce complexity." },
+  { question: "What is polymorphism? (OOP)", answer: "Polymorphism describes situations in which something occurs in several different forms. It describes the concept that you can access objects of different types through the same interface and each type can provide its own independent implementation of this interface." },
+  { question: "What is inheritance? (OOP)", answer: "Inheritance is a concept in which the 'child' class will inherit all the public and protected properties and methods/behaviors from the 'parent' class. In addition, it can have its own properties and methods." },
+  { question: "What is encapsulation? (OOP)", answer: "Encapsulation refers to the bunching of data and code into a single unit. A common example of encapsulation are classes in OOP (object-oriented programming) languages. Classes contain both variables (data) and behaviours (functions/methods)." },
+];
+
+let generalStart = 0;
+let generalEnd = 12;
+let oopStart = 13;
+let oopEnd = 19;
+
+/*had to put the window onload as otherwise the console throws an error as it executes before page is loaded*/
+window.onload=function() {
+const startButton = document.getElementById('start-btn');
+startButton.addEventListener('click', startGame);
+}
+
+let valid_questions = [];
+
+function startGame() {
+  /*should first check which boxes are checked and set up the valid list of quesitons this way*/
+  if (document.getElementById('one').checked) {
+    for (let i = oopStart; i <= oopEnd; i++) {
+      valid_questions.push(i);
+    }
+  }
+
+  if (document.getElementById('two').checked) {
+    for (let i = generalStart; i <= generalEnd; i++) {
+      valid_questions.push(i);
+    }
+  }
+
+  console.log(valid_questions);
+  fraction()
+  document.getElementsByClassName("overlay")[0].style.display = "none"
+};
+
+function fraction() {
+  document.getElementById('denom').innerHTML = valid_questions.length;
+  document.getElementById('numer').innerHTML = used_questions.length;
+}
